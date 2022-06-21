f5018c9a242ee1f31cd59e5b078a8dcc043036aa
trust0212 <sergeyfedorenkodev@outlook.com>
2022-06-21 09:45:48 -0400

    add usedapp

--- src/index.tsx
+++ src/index.tsx
@@ -3,10 +3,21 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { Config, DAppProvider } from '@usedapp/core';
+import { NETWORK_ID, RPC_URL, DAPP_READONLY_RPC } from './config';
+
+const config: Config = {
+  readOnlyChainId: NETWORK_ID,
+  readOnlyUrls: {
+    [NETWORK_ID]: DAPP_READONLY_RPC,
+  },
+}
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+     <DAppProvider config={config}>
+      <App />
+    </DAppProvider>
   </React.StrictMode>,
   document.getElementById('root')
 );
